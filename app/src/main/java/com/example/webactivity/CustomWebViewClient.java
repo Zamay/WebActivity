54f36100d977bf07c39d55c869b1a576f1f470b4
zamay <mityazamay@gmail.com>
2019-10-25 17:49:01 +0300

    ...

--- app/src/main/java/com/example/webactivity/CustomWebViewClient.java
+++ app/src/main/java/com/example/webactivity/CustomWebViewClient.java
@@ -1,10 +1,15 @@
 package com.example.webactivity;
 
 import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Build;
+import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 class CustomWebViewClient extends WebViewClient {
     @TargetApi(Build.VERSION_CODES.N)
@@ -20,4 +25,10 @@ class CustomWebViewClient extends WebViewClient {
         view.loadUrl(url);
         return true;
     }
+
+    @Override
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        handler.proceed();
+    }
+
 }
--- app/src/main/java/com/example/webactivity/MainActivity.java
+++ app/src/main/java/com/example/webactivity/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.webactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -21,23 +24,26 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static com.example.webactivity.ConnectivityHelper.hasActiveInternetConnection;
-import static com.example.webactivity.ConnectivityHelper.isNetworkConnected;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    private boolean backPressedOnce = false;
     private Handler statusUpdateHandler = new Handler();
     private Runnable statusUpdateRunnable;
-    private ProgressBar spinner;
+    public ProgressBar spinner;
+    private RequestQueue queue;
+    private SharedPreferences mSettings;
+    private SharedPreferences.Editor editor;
 
+    private static final String PACKAGE_NAME = BuildConfig.APPLICATION_ID;
+    private static final String APP_PREFERENCES = "mysettings";
+    private static final String APP_PREFERENCES_W = "whitePage";
+
+    private boolean backPressedOnce = false;
+    private boolean whitePage = false;
     private static String promoType, promoUrl;
-    private static String PACKAGE_NAME = BuildConfig.APPLICATION_ID;
-
-    public static String url = "http://android-app-rest.zinenko.net/api/entry?package=" + PACKAGE_NAME;
-
-    public RequestQueue queue;
+    private static final String whitePageUrl = "file:///android_asset/index.html";
+    private static final String url = "https://android-app-rest.pencompcock.online/params?id=" + PACKAGE_NAME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,23 +54,53 @@ public class MainActivity extends AppCompatActivity {
         spinner.setVisibility(View.VISIBLE);
 
         queue = Volley.newRequestQueue(this);
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        editor = mSettings.edit();
 
         webView = findViewById(R.id.webView);
+
+        if(mSettings.contains(APP_PREFERENCES_W)) {
+            whitePage = mSettings.getBoolean(APP_PREFERENCES_W, false);
+            Log.d("whitePage", "... " + whitePage);
+        }
+        webViewSettings();
+
+        if(whitePage) {
+            Log.d("test", "whitePage true");
+            webView.loadUrl(whitePageUrl);
+        } else {
+            Log.d("test", "whitePage false");
+            stringRequestGet();
+            editor.putBoolean(APP_PREFERENCES_W, true);
+            editor.apply();
+        }
+    }
+
+    private void webViewSettings() {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setAllowFileAccess(true);
-        webView.getSettings().getUserAgentString();
-        webView.setWebViewClient(new CustomWebViewClient());
-
-        webView.setWebViewClient(new WebViewClient() {
-            public void onPageFinished(WebView view, String url) {
+//        webView.getSettings().getUserAgentString();
+        webView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19");
+//        webView.getSettings().setAppCachePath("/data/data/" + getPackageName() + "/cache");
+        webView.getSettings().setAppCachePath(getCacheDir().getAbsolutePath() + "/cache");
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.setWebViewClient(new CustomWebViewClient(){
+            @Override public void onPageFinished(WebView view, String url) {
                 spinner.setVisibility(View.GONE);
+                webView.saveWebArchive("file://test.mht");
+                Toast.makeText(getApplicationContext(), "Страница загружена!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                Log.d("url", url);
+                Toast.makeText(getApplicationContext(), "Начата загрузка страницы", Toast.LENGTH_SHORT)
+                        .show();
             }
         });
-
-//        checkConnection();
-
-        stringRequestGet();
     }
 
     private void stringRequestGet() {
@@ -84,40 +120,31 @@ public class MainActivity extends AppCompatActivity {
         queue.add(stringRequest);
     }
 
-    public void parseData(String response) {
+    private void parseData(String response) {
         try {
             JSONObject jsonObject = new JSONObject(response);
             promoType = jsonObject.getString("promoType");
             promoUrl = jsonObject.getString("promoUrl");
-            String responseUrl = "";
+            String loadUrl = "";
 
             if (promoType.equals("l")) {
-                responseUrl = promoUrl;
+                loadUrl = promoUrl;
             } else if (promoType.equals("p")) {
-                responseUrl = promoUrl;
+                loadUrl = promoUrl;
             } else if (promoType.equals("w")) {
-                responseUrl = "file:///android_asset/game-js.html";
+                whitePage = true;
+                loadUrl = whitePageUrl;
+                editor.putBoolean(APP_PREFERENCES_W, whitePage);
+                editor.apply();
             }
 
-//            Log.d("url", responseUrl);
-            webView.loadUrl(responseUrl);
+//            Log.d("url", loadUrl);
+            webView.loadUrl(loadUrl);
         } catch (JSONException e) {
             Log.e("parseDataError", e.getMessage());
             e.printStackTrace();
         }
     }
-
-//    public void checkConnection() {
-//
-//        if(hasActiveInternetConnection(getApplicationContext())) {
-//            Log.d("internet status","Internet Access");
-//            stringRequestGet();
-//        } else {
-//            Log.d("internet status","no Internet Access");
-//            Toast.makeText(getApplicationContext(),
-//                    "Отсутствует интернет соединение", Toast.LENGTH_SHORT).show();
-//        }
-//    }
 
     public void onBackPressed() {
         if (webView.canGoBack()) {
